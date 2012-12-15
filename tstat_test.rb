a3c9b5c6ce311ba5fe9935b8d64420def930e459
Michael O'Keefe <okeefm@rpi.edu>
2012-12-15 13:57:07 -0500

    Updated tstat_test to test ruby_tstat

--- tstat_test.rb
+++ tstat_test.rb
@@ -6,7 +6,11 @@ TSTAT_IP = 'http://okeefm.dyndns-home.com:8888'
 tstat = Tstat.new(TSTAT_IP)
 
 # Show current setpoint
-puts "Current target: #{ tstat.get_heat_target.to_s }"
+puts "Current target: #{ tstat.get_setpoint.to_s }"
 
 # Get current temp (automatically parses JSON to Ruby hash)
 puts "Current indoor temp: #{ tstat.get_current_temp.to_s}"
+
+#Get today's/yesterday's system usage (today's heat by default)
+usage = tstat.get_usage
+puts "Yesterday's heat usage: #{ usage.to_s}"
